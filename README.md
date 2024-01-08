0012bc257b4ed337edde5a33e2c2a5190bc8605f
Pia Mendoza <lmendoza22@my.bcit.ca>
2024-01-08 12:22:54 -0800

    Adding exercise-2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Exercise 2
+## Created by
+- Pia
+## Description
+- adding reusable components (buttons, footer, header, and counter)
+- placing a useState
---
+++ components/Footer/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "./Footer.module.css"
+
+export default function Footer() {
+    return(
+        <footer className={styles.footer}>
+            <p>Created by Pia Mendoza</p>
+        </footer>
+    )
+}
